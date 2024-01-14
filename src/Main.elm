7ff959dcb0d49e214ec50414af01591bdd0d6e2e
calvinseptyanto <98633109+calvinseptyanto@users.noreply.github.com>
2024-01-14 17:10:19 +0800

    Fixed minor errors on decoding

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,9 @@
 port module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, pre, text)
-import Html.Attributes exposing (style)
-import Html.Events exposing (onClick)
+import Html exposing (Html, div, pre, text, input, button)
+import Html.Attributes exposing (type_, style)
+import Html.Events exposing (onInput, onClick)
 import Json.Decode as Decode
 import Json.Encode as Encode
 
@@ -20,59 +20,82 @@ main =
 -- MODEL
 
 type alias Model =
-    { jsonResult : String
+    { rawLines : List String
+    , parsedCsv : String
+    , jsonResult : String
     }
 
+-- Define the initial model state
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { jsonResult = "" }
+    ( { rawLines = []
+      , parsedCsv = ""
+      , jsonResult = ""
+      }
     , Cmd.none
     )
 
 -- UPDATE
 
 type Msg
-    = FileSelected String
-    | FileRead String -- Changed to String to receive raw JSON
+    = SetRawLines (List String)
+    | SetParsedCsv String
+    | SetJsonResult String
 
+-- Define how the model should be updated in response to messages
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        FileSelected content ->
-            ( model
-            , checkFile content
+        SetRawLines lines ->
+            ( { model | rawLines = lines }
+            , Cmd.none
             )
 
-        FileRead jsonString ->
-            -- Decode jsonString inside Elm now
-            case Decode.decodeString Decode.value jsonString of
-                Ok value ->
-                    ( { model | jsonResult = Encode.encode 2 value }
-                    , Cmd.none
-                    )
+        SetParsedCsv csv ->
+            ( { model | parsedCsv = csv }
+            , Cmd.none
+            )
 
-                Err error ->
-                    ( { model | jsonResult = "Could not decode JSON from the file: " ++ Decode.errorToString error }
-                    , Cmd.none
-                    )
+        SetJsonResult json ->
+            ( { model | jsonResult = json }
+            , Cmd.none
+            )
 
 -- VIEW
 
+-- Define how to render the model as HTML
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick (FileSelected "") ] [ text "Click to select a CSV file." ]
-        , div [] [ pre [] [ text model.jsonResult ] ]
+        [ div [] [ text "First Ten Lines of Raw Input:" ]
+        , pre [] [ text (String.join "\n" (List.take 10 model.rawLines)) ]
+        , div [] [ text "Parsed CSV:" ]
+        , pre [] [ text model.parsedCsv ]
+        , div [] [ text "JSON Output:" ]
+        , pre [] [ text model.jsonResult ]
+        , input [ type_ "file", onInput (SetRawLines []), style [ ( "display", "none" ) ] ] []
+        , button [ onClick (SetJsonResult "") ] [ text "Download JSON" ]
         ]
-
+        
 -- SUBSCRIPTIONS
 
+-- Define subscriptions for your app (none in this case)
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    convertComplete FileRead
+    Sub.none
 
 -- PORTS
 
-port checkFile : String -> Cmd msg
+-- Define outbound ports to send data out of Elm
+port checkFile : List String -> Cmd msg
 
-port convertComplete : (String -> msg) -> Sub msg
+port parseCsv : String -> Cmd msg
+
+port downloadJson : Encode.Value -> Cmd msg
+
+-- HELPER FUNCTIONS
+
+-- A helper function to convert JSON values to a formatted string
+convertJsonToString : Encode.Value -> String
+convertJsonToString jsonValue =
+    Encode.encode 2 jsonValue
