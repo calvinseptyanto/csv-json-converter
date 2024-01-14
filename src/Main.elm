5219ec4c3715b09ed9c3dcde4db7900ebb7b285e
calvinseptyanto <98633109+calvinseptyanto@users.noreply.github.com>
2024-01-14 16:34:50 +0800

    Add main

---
+++ src/Main.elm
@@ -0,0 +1,19 @@
+port module Main exposing (..)
+
+import Browser
+import Html exposing (Html, button, div, pre, text)
+import Html.Attributes exposing (style)
+import Html.Events exposing (onClick)
+import Json.Decode as Decode
+import Json.Encode as Encode
+
+
+-- MAIN
+
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
